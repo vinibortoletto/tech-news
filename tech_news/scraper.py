a76e1c44f655e9b4e9780a28ca02364c3ec628fe
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-16 16:39:44 -0300

    feat: scrape news info

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,7 +36,36 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+
+    reading_time = int(
+        selector.css(".meta-reading-time::text").get().split(" ")[0]
+    )
+
+    summary = selector.css(".entry-content p::text").get()
+
+    category = selector.css(
+        ".meta-category .category-style .label::text"
+    ).get()
+
+    summary = "".join(
+        selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+    ).strip()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
