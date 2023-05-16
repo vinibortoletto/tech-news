e57acabfd18ff52b98a89792c53997380f7c3877
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-16 19:02:21 -0300

    feat: search news by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -11,7 +12,19 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    unformatted_date = datetime.strptime(date, "%Y-%m-%d")
+    formatted_date = datetime.strftime(unformatted_date, "%d/%m/%Y")
+
+    query = {"timestamp": formatted_date}
+    news_list = search_news(query)
+    news_dict = [(news["title"], news["url"]) for news in news_list]
+
+    return news_dict
 
 
 # Requisito 9
