353c59e93fd539bb2198fa6d6e48a79f28118cc9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-16 23:09:17 -0300

    refactor: organize options messages

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -11,10 +11,13 @@ import sys
 # Requisitos 11 e 12
 def analyzer_menu():
     options_message = (
-        "Selecione uma das opções a seguir:\n 0 - Popular o banco com notícias"
-        ";\n 1 - Buscar notícias por título;\n 2 - Buscar notícias por data;\n"
-        " 3 - Buscar notícias por categoria;\n "
-        "4 - Listar top 5 categorias;\n 5 - Sair."
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por categoria;\n"
+        " 4 - Listar top 5 categorias;\n"
+        " 5 - Sair."
     )
 
     try:
