bbefd8385fa384c2b50ea5e2e37f3fa2ef46a821
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-16 22:29:22 -0300

    feat: get news by amount menu option

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,25 @@
+from tech_news.scraper import get_tech_news
+import sys
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    options_message = (
+        "Selecione uma das opções a seguir: \n"
+        "0 - Popular o banco com notícias; \n"
+        "1 - Buscar notícias por título; \n"
+        "2 - Buscar notícias por data; \n"
+        "3 - Buscar notícias por categoria; \n"
+        "4 - Listar top 5 categorias; \n"
+        "5 - Sair. \n"
+    )
+
+    try:
+        selected_option = int(input(options_message))
+    except ValueError:
+        sys.stderr.write("Opção inválida")
+    else:
+        if selected_option == 0:
+            news_amount = int(input("Digite quantas notícias serão buscadas:"))
+            test = get_tech_news(news_amount)
+            sys.stderr.write(test)
