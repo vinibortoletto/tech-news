ee8e0aec0c44dd4da5d37af17a0bffab0b1af2a6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-16 23:25:15 -0300

    refactor: split main function to subfunctions

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -8,6 +8,43 @@ from tech_news.analyzer.search_engine import (
 import sys
 
 
+def handle_search_options(option):
+    news_list = None
+
+    if option == 1:
+        news_title = input("Digite o título:")
+        news_list = search_by_title(news_title)
+
+    elif option == 2:
+        news_date = input("Digite a data no formato aaaa-mm-dd:")
+        news_list = search_by_date(news_date)
+
+    elif option == 3:
+        news_category = input("Digite a categoria:")
+        news_list = search_by_category(news_category)
+
+    return news_list
+
+
+def handle_options(option):
+    news_list = None
+
+    if option == 0:
+        news_amount = int(input("Digite quantas notícias serão buscadas:"))
+        news_list = get_tech_news(news_amount)
+
+    elif option in [1, 2, 3]:
+        handle_search_options(option)
+
+    elif option == 4:
+        news_list = top_5_categories()
+
+    elif option == 5:
+        sys.stdout.write("Encerrando script\n")
+
+    return news_list
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
     options_message = (
@@ -21,31 +58,11 @@ def analyzer_menu():
     )
 
     try:
-        selected_option = int(input(options_message))
+        option = int(input(options_message))
         news_list = None
 
-        if selected_option == 0:
-            news_amount = int(input("Digite quantas notícias serão buscadas:"))
-            news_list = get_tech_news(news_amount)
-
-        elif selected_option == 1:
-            news_title = input("Digite o título:")
-            news_list = search_by_title(news_title)
-
-        elif selected_option == 2:
-            news_date = input("Digite a data no formato aaaa-mm-dd:")
-            news_list = search_by_date(news_date)
-
-        elif selected_option == 3:
-            news_category = input("Digite a categoria:")
-            news_list = search_by_category(news_category)
-
-        elif selected_option == 4:
-            news_list = top_5_categories()
-
-        elif selected_option == 5:
-            sys.stdout.write("Encerrando script\n")
-
+        if option in range(6):
+            handle_options(option)
         else:
             raise ValueError
 
